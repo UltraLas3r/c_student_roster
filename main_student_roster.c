9e67367bd975ffa6aa4fe088f9367867b79747b6
120864 <michael.schreiber@sncorp.com>
2025-03-03 09:17:50 -0800

    initial commit and welcome screen

---
+++ main_student_roster.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+
+    welcomeScreen();
+
+    return 0;
+}
+
+void welcomeScreen(){
+    printf("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n");
+    printf("Hello, Welcome to the Center for Kids Who want to learn to read good but cant read good!");
+    printf("\n ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n");
+}
